8e143fe0bffe18cb9c82b8eea1aeceb372857717
DieppDiepp <23521041@gm.uit.edu.vn>
2024-04-30 15:10:54 +0700

    Third Commit

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#if 1
+
+#include <bits/stdc++.h>
+#define BOOST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define maxn 100005
+
+typedef long long int ll;
+const double Pi = acos(- 1.0);
+
+using namespace std;
+
+int main() {
+	BOOST;
+	cout<<"Helo world";
+	return 0;
+}
+#endif
